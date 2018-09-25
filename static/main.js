b2b932ed13ddf9054485533a09109edc953f9a38
AnimiVulpis <animi.vulpis@gmail.com>
2018-09-25 15:40:29 +0300

    Start vertical steam slice

--- static/main.js
+++ static/main.js
@@ -1,1 +1,15 @@
-const steamGamesList = document.querySelector('#steam-games-html')
+const steamHtmlInput = document.querySelector('#steam-games-html-input')
+
+function extractSteamGamesFromHtml() {
+
+}
+
+// Will-be code beyond this point
+
+// showFileSelectorForX
+// (On Drag & Drop) show different areas for different libraries
+// Maybe multiple and maybe detect library type automatically
+
+// readData (async and with progress bar)
+
+// showGameList
