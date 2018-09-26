b0255908f1990169b9f33357c5b103b6137a410b
AnimiVulpis <animi.vulpis@gmail.com>
2018-09-27 01:21:36 +0300

    Add working data extraction
    While including a lot of open TODOs

--- static/main.js
+++ static/main.js
@@ -1,7 +1,29 @@
 const steamHtmlInput = document.querySelector('#steam-games-html-input')
+const reader = new FileReader()
+
+// TODO: Think about architecture of this application
 
 function extractSteamGamesFromHtml() {
+  reader.readAsText(steamHtmlInput.files[0])
+}
 
+function extractData() {
+  const domParser = new DOMParser()
+  // TODO: Add error handling
+  const steamHtml = domParser.parseFromString(reader.result, 'text/html')
+  // Get the script element with an `language` attribute
+  // TODO: Check for the correct `script` tag?
+  const result = steamHtml.querySelector('script[language]')
+  // Get the correct line of the game list definition
+  // TODO: Check for the correct line?
+  .text.split('\n')[1]
+  // Cut off everything that is **not** the array of games
+  // TODO: Use regex to extract array (avoiding additional `slice`)
+  .split('rgGames = ')[1]
+  // Removing the trailing `;`
+  .slice(0, -1)
+  // Returning the raw array of objects
+  return JSON.parse(result)
 }
 
 // Will-be code beyond this point
